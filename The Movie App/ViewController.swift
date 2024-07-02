8b5c62515dcd5942b7a7e9d16a9855c88503d358
nitesh8059685@ <nitesh8059685@gmail.com>
2024-07-02 21:36:47 +0530

    Initial Commit

---
+++ The Movie App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Movie App
+//
+//  Created by Nitesh Sharma on 02/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
